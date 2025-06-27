60efdb4cf3846f23e18a1b602dac484fc9f70dfc
Karl Beckman <karlbeckman97@gmail.com>
2025-06-27 22:54:30 +0200

    Fix: now runs om correct post. Also CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from dotenv import load_dotenv
 import os
@@ -16,6 +17,7 @@ db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
+  CORS(app)
 
   app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
